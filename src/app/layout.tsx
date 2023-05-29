a8d0b427cf5a8c210b431845b9e7bf8ce5676ffa
Emmett Hoolahan <emmetthoolahan@gmail.com>
2023-05-29 14:33:59 +0800

    poppins font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
     <head>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" /> 
     </head>
       <body className={inter.className}>{children}</body>
     </html>
