b8ea081ccceb47e1a37d563326b2b833ae9c6263
Emmett Hoolahan <emmetthoolahan@gmail.com>
2023-05-29 16:10:47 +0800

    accordion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,17 +8,17 @@ export default function Home() {
       <h1 className="text-3xl text-center mb-8">A digital agency that is anything but ordinary</h1>
       <div className="z-10 w-full max-w-5xl items-center justify-center font-mono text-sm">
         <div className="about mb-8">
-          <h2 className="mb-4">About</h2>
+          <h2 className="text-2xl mb-4">About</h2>
           <p>Our digital agency excels in web development, SEO, API integrations, marketing, AI, and marketing strategy. We create stunning websites, optimize them for search engines, and seamlessly integrate different software systems. Our marketing services leverage AI to analyze data and devise effective strategies. We're dedicated to delivering tangible results for Australian businesses in the digital realm.</p>
         </div>
 
         <div className="team mb-8">
-          <h2 className="mb-4">Team</h2>
+          <h2 className="text-2xl mb-4">Team</h2>
           <p>We are comprising of both local and international talent. Our agency is proud to have a group of professionals with diverse expertise and backgrounds. Many of our team members bring valuable experience from agency backgrounds in Perth, Western Australia.</p>
         </div>
 
         <div className="services mb-8">
-          <h2 className="mb-4">Our Services</h2>
+          <h2 className="text-2xl mb-4">Our Services</h2>
           <Accordion question="Web Support & Hosting" answer="We provide excellent web support and hosting services." />
           <Accordion question="SEO & PPC" answer="Our SEO and PPC services are designed to help you reach your target audience effectively." />
           <Accordion question="Automations & AI" answer="We implement cutting-edge automations and AI to boost the efficiency and effectiveness of your business processes." />
@@ -26,7 +26,7 @@ export default function Home() {
                   </div>
 
         <div className="work mb-8">
-          <h2 className="mb-4">Our Work</h2>
+          <h2 className="text-2xl mb-4">Our Work</h2>
           <Accordion question="JLC Associates" answer="We rebuilt their website giving them a clean modern design. Check out the website at <a href='http://jlcassociates.com.au' target='_blank' rel='noopener noreferrer'>jlcassociates.com.au</a>." />
           <Accordion question="The Barefoot Disciple" answer="Details of the work we've done for The Barefoot Disciple." />
           <Accordion question="Instant Fun" answer="We rebuilt their website in Shopify, offering a modern ecommerce experience." />
