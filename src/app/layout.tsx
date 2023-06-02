e9fda16677dcad68881161fbcceda1c6efa5f40c
Emmett Hoolahan <emmetthoolahan@gmail.com>
2023-06-02 12:17:36 +0800

    img chat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+"use client";
+
 import './globals.css'
 import { Inter } from 'next/font/google'
-
+import { ParallaxProvider } from 'react-scroll-parallax';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -18,7 +20,9 @@ export default function RootLayout({
     <head>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
     </head>
+        <ParallaxProvider>
       <body className={inter.className}><div id="my-root">{children}</div></body>
+      </ParallaxProvider>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Accordion from './components/Accordion';
 import { FiPhone } from 'react-icons/fi';
 import { useInView } from 'react-intersection-observer';
 import ChatWidget from './components/ChatWidget';
-
+import { Parallax } from 'react-scroll-parallax';
 
 const variants = {
   hidden: { opacity: 0, x: -100 },
@@ -89,8 +89,8 @@ export default function Home() {
   };
 
   return (
-    <main className="flex flex-col min-h-screen items-center justify-start pt-28 md:pt-20 p-4 sm:p-6 md:p-8 lg:p-12 xl:p-28 bg-white text-black">
-    <header className={`fixed top-0 right-0 bg-white p-4 md:p-8 w-full z-10 ${isScrolled ? '' : ''}`}>
+    <main className="flex flex-col min-h-screen bg-white items-center justify-start pt-28 md:pt-20 p-4 sm:p-6 md:p-8 lg:p-12 xl:p-28 bg-transparent text-black">
+<header className={`fixed top-0 right-0 bg-transparent p-4 md:p-8 w-full z-10 ${isScrolled ? '' : ''}`}>
   <div className="flex items-center justify-between w-full">
     <Logo className="w-12 h-12 md:w-16 md:h-16 object-contain" />
     <div className="md:flex items-center">
@@ -133,28 +133,37 @@ export default function Home() {
         <button onClick={closeModal} className="mt-4 w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-gray-600 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">Close</button>
       </Modal>
 
-      <div className="flex flex-col md:flex-row justify-center items-center text-4xl md:text-8xl font-extrabold my-0 h-screen bg-white text-black mt-8 md:mt-0">
-    <motion.div className="flex flex-col justify-center items-center h-full w-full p-0">
-      <motion.span className="md:pr-4 text-5xl sm:text-6xl md:text-8xl"
-        style={{ fontFamily: "'Poppins', sans-serif" }}
-        initial={{ opacity: 0, x: -100 }}
-        animate={{ opacity: 1, x: 0 }}
-        transition={{ duration: 1 }}
-      >
-        Anything.
-      </motion.span>
-      <motion.span className="text-5xl sm:text-6xl md:text-8xl"
-        style={{ fontFamily: "'Poppins', sans-serif" }}
-        initial={{ opacity: 0, x: 100 }}
-        animate={{ opacity: 1, x: 0 }}
-        transition={{ duration: 1 }}
-      >
-        But Ordinary
-      </motion.span>
-    </motion.div>
+<Parallax y={[-100, 100]} tagOuter="figure">
+    <div className="flex flex-col hero md:flex-row justify-center items-center text-4xl md:text-8xl font-extrabold my-0 h-screen bg-white text-black mt-8 md:mt-0"
+      style={{
+        backgroundImage: "linear-gradient(rgba(255, 255, 255, 0), rgba(255, 255, 255, 0.5)), url('/images/perthhigh.png')",
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundAttachment: 'fixed',
+        height: '100vh',
+        width: '100vw'
+      }}
+    >
+  <motion.span className="md:pr-4 text-5xl sm:text-6xl md:text-8xl text-black"
+  style={{ fontFamily: "'Poppins', sans-serif" }}
+  initial={{ opacity: 0, x: -100 }}
+  animate={{ opacity: 1, x: 0 }}
+  transition={{ duration: 1 }}
+>
+  Anything.
+</motion.span>
+<motion.span className="text-5xl sm:text-6xl md:text-8xl text-black"
+  style={{ fontFamily: "'Poppins', sans-serif" }}
+  initial={{ opacity: 0, x: 100 }}
+  animate={{ opacity: 1, x: 0 }}
+  transition={{ duration: 1 }}
+>
+  But Ordinary
+</motion.span>
   </div>
-
-      <div ref={refAbout} className="z-10 w-full max-w-5xl items-center justify-center font-mono text-sm">
+</Parallax>
+      <div ref={refAbout} className="z-10 pt-12 w-full max-w-5xl items-center justify-center font-mono text-sm">
         <motion.div className="about mb-8"
           initial="hidden"
           animate={inViewAbout ? 'visible' : 'hidden'}
