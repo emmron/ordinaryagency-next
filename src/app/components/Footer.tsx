ffbbefb594414d79857497f6aeb672b51e4ebe0d
Emmett Hoolahan <emmetthoolahan@gmail.com>
2023-06-06 16:19:54 +0800

    footer instagram responsive

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 import Logo from './Logo';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   const footerRef = useRef(null);
@@ -34,10 +34,13 @@ const Footer = () => {
       <div className="flex justify-between items-center px-4 md:px-8">
         <Logo className="w-12 h-12 md:w-16 md:h-16 object-contain" />
         <div className="text-sm">
-          <a href="/terms" className="mr-4 bg-white border border-black text-black hover:bg-black hover:text-white rounded-full px-4 py-2 transition-colors duration-200 ease-in-out font-poppins hover:animate-bounce">Terms</a>
-          <a href="/privacy" className="mr-4 bg-white border border-black text-black hover:bg-black hover:text-white rounded-full px-4 py-2 transition-colors duration-200 ease-in-out font-poppins hover:animate-bounce">Privacy Policy</a>
-          <a href="https://au.linkedin.com/company/ordinaryagencyseo?trk=public_profile_experience-item_profile-section-card_subtitle-click" className="bg-white border border-black text-black hover:bg-black hover:text-white rounded-full px-4 py-2 transition-colors duration-200 ease-in-out font-poppins hover:animate-bounce">
+          <a href="/terms" className="mr-2 md:mr-4 bg-white border border-black text-black hover:bg-black hover:text-white rounded-full px-2 md:px-4 py-1 md:py-2 transition-colors duration-200 ease-in-out font-poppins hover:animate-bounce">Terms</a>
+          <a href="/privacy" className="mr-2 md:mr-4 bg-white border border-black text-black hover:bg-black hover:text-white rounded-full px-2 md:px-4 py-1 md:py-2 transition-colors duration-200 ease-in-out font-poppins hover:animate-bounce">Privacy Policy</a>
+          <a href="https://au.linkedin.com/company/ordinaryagencyseo?trk=public_profile_experience-item_profile-section-card_subtitle-click" className="mr-2 md:mr-4 bg-white border border-black text-black hover:bg-black hover:text-white rounded-full px-2 md:px-4 py-1 md:py-2 transition-colors duration-200 ease-in-out font-poppins hover:animate-bounce">
             <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          <a href="https://www.instagram.com/ordinaryagency/" className="bg-white border border-black text-black hover:bg-black hover:text-white rounded-full px-2 md:px-4 py-1 md:py-2 transition-colors duration-200 ease-in-out font-poppins hover:animate-bounce">
+            <FontAwesomeIcon icon={faInstagram} />
           </a>
         </div>
       </div>
