67660ee86ee9a7a52e61d77b7c69e75f5bdb2c5f
Emmett Hoolahan <emmetthoolahan@gmail.com>
2023-06-22 22:59:18 +0800

    seo-perth scrolling img

--- src/app/components/seosection.js
+++ src/app/components/seosection.js
@@ -1,40 +1,28 @@
-import { motion } from 'framer-motion';
+import { motion, useViewportScroll, useTransform } from 'framer-motion';
 
-const backdrop = {
-  visible: { opacity: 1 },
-  hidden: { opacity: 0 },
-};
+const easing = [0.6, -0.05, 0.01, 0.99];
 
-const contentVariants = {
-  hidden: { x: '-100vw' },
-  visible: {
-    x: 0,
-    transition: {
-      type: 'spring',
-      stiffness: 30,
-      damping: 10
-    }
-  },
-};
+const SEOSection = () => {
+  const { scrollYProgress } = useViewportScroll();
+  const cardOpacity = useTransform(scrollYProgress, [0, 0.3], [0, 1]);
+  const cardX = useTransform(scrollYProgress, [0, 0.1, 0.4], ['-100vw', '-45vw', '0vw'], easing);
 
-const SEOSection = () => (
-  <motion.section
-    className="relative flex items-center justify-center min-h-screen py-20 px-4 bg-center bg-cover"
-    style={{ backgroundImage: `url(/images/0_0.png)`}}
-    variants={backdrop}
-    initial="hidden"
-    animate="visible"
-  >
-    <motion.div
-      className="relative z-10 max-w-3xl p-10 mx-auto bg-gradient-to-br from-black to-gray-800 bg-opacity-70 rounded-xl shadow-2xl text-white"
-      variants={contentVariants}
+  return (
+    <section
+      className="relative flex items-center justify-center min-h-screen py-10 sm:py-20 px-2 sm:px-4 bg-center bg-cover"
+      style={{ backgroundImage: `url(/images/0_0.png)`}}
     >
-      <h2 className="mb-6 text-4xl font-extrabold tracking-wide sm:text-5xl">What is SEO and Why it's Crucial for Your Business</h2>
-      <p className="text-lg text-white font-light tracking-wide leading-relaxed">
-        Search engine optimization, or SEO, is the process of enhancing a website's visibility on search engine results pages (SERPs). By leveraging SEO, Perth businesses can increase their online visibility, reach more potential customers, and improve their bottom line. SEO is crucial for businesses of all sizes because it helps attract organic traffic, build credibility, and deliver a better user experience.
-      </p>
-    </motion.div>
-  </motion.section>
-);
+      <motion.div
+        className="relative z-10 max-w-xs sm:max-w-3xl p-6 sm:p-10 mx-auto bg-white bg-opacity-80 rounded-xl shadow-2xl"
+        style={{ opacity: cardOpacity, x: cardX }}
+      >
+        <h2 className="mb-4 sm:mb-6 text-xl sm:text-4xl font-extrabold tracking-wide text-gray-900">What is SEO and Why it's Crucial for Your Business</h2>
+        <p className="text-sm sm:text-lg font-light tracking-wide leading-relaxed text-gray-800">
+          Search engine optimization, or SEO, is the process of enhancing a website's visibility on search engine results pages (SERPs). By leveraging SEO, Perth businesses can increase their online visibility, reach more potential customers, and improve their bottom line. SEO is crucial for businesses of all sizes because it helps attract organic traffic, build credibility, and deliver a better user experience.
+        </p>
+      </motion.div>
+    </section>
+  );
+};
 
 export default SEOSection;
