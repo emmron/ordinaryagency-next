e534b1a2e474a5e24d8568c8c7d396c6f04df4b3
Emmett Hoolahan <emmetthoolahan@gmail.com>
2023-08-29 19:07:28 +0800

    Updated PlayHQPerth page to be more visually appealing

--- src/pages/shopify-perth.tsx
+++ src/pages/shopify-perth.tsx
@@ -28,18 +28,18 @@ export default function ShopifyPerth() {
       </Head>
       <main id="#my-root" className="flex flex-col min-h-screen bg-white items-center justify-start pt-28 md:pt-20 p-4 sm:p-6 md:p-8 lg:p-12 lg:py-0 xl:p-28 bg-transparent text-black">
         <Header />
-        <div>
+        <div className="container mx-auto px-4 py-12 text-center">
           <motion.div className="services mb-8"
             initial="hidden"
             animate="visible"
             variants={variants}
             transition={{ delay: 1.5 }}
           >
-            <h2 className="text-2xl mb-4">Our Shopify Services</h2>
+            <h2 className="text-5xl font-extrabold mb-4 text-black tracking-wide transform transition-transform duration-500 hover:scale-105">Our Shopify Services</h2>
             <Accordion
               question="Shopify Store Setup & Customization"
               answer={
-                <div>
+                <div className="text-xl text-black italic mb-12 transform transition-transform duration-500 hover:scale-105">
                   We help businesses set up their Shopify stores and customize them to match their brand. This includes setting up product listings, payment gateways, shipping settings, and more.
                 </div>
               }
@@ -48,7 +48,7 @@ export default function ShopifyPerth() {
             <Accordion
               question="Shopify SEO"
               answer={
-                <div>
+                <div className="text-xl text-black italic mb-12 transform transition-transform duration-500 hover:scale-105">
                   We optimize your Shopify store for search engines to help you rank higher, attract more traffic, and increase sales.
                 </div>
               }
@@ -57,7 +57,7 @@ export default function ShopifyPerth() {
             <Accordion
               question="Shopify Maintenance & Support"
               answer={
-                <div>
+                <div className="text-xl text-black italic mb-12 transform transition-transform duration-500 hover:scale-105">
                   We provide ongoing maintenance and support services to keep your Shopify store running smoothly. This includes updating product listings, managing promotions, troubleshooting issues, and more.
                 </div>
               }
