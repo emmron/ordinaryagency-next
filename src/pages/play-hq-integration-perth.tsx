84b8d45b4c4295b140951aba1fcdebe8846108e8
Emmett Hoolahan <emmetthoolahan@gmail.com>
2023-08-29 19:11:23 +0800

    Updated PlayHQPerth page to be more visually appealing

--- src/pages/play-hq-integration-perth.tsx
+++ src/pages/play-hq-integration-perth.tsx
@@ -5,7 +5,7 @@ import Header from "../app/components/Header";
 import Footer from "../app/components/Footer";
 import Head from "next/head";
 import InternalHero from "../app/components/InternalHero";
-import { FaChartLine, FaRegLightbulb, FaRocket, FaSearchDollar, FaCheck } from "react-icons/fa";
+import { FaBasketballBall, FaCalendarAlt, FaWhistle } from "react-icons/fa";
 import { motion } from 'framer-motion';
 
 const PlayHQPerth = () => {
@@ -27,23 +27,23 @@ const PlayHQPerth = () => {
         <section className="container mx-auto px-4 py-12 text-center">
           <img src="/images/hq.png" alt="Play HQ Integration" className="w-64 h-auto mx-auto mb-8" />
           <h2 className="text-5xl font-extrabold mb-4 text-black tracking-wide transform transition-transform duration-500 hover:scale-105">Empowering Perth Sports Clubs with Play HQ Integration</h2>
-          <p className="text-xl text-black italic mb-12 transform transition-transform duration-500 hover:scale-105">We help you streamline your club's operations and enhance member experience with our top integration services in Perth. Experience the power of seamless integration with Play HQ.</p>
+          <p className="text-xl text-black italic mb-12 transform transition-transform duration-500 hover:scale-105">With Play HQ integration, you can easily add fixtures and season stats to your website, making it easy for your members to stay up-to-date.</p>
 
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
             <div className="integration-item text-left p-6 md:p-10 space-y-4 transition-all duration-500 ease-in-out transform hover:shadow-2xl hover:scale-105">
-                <FaSearchDollar className="text-black transition-all duration-500 ease-in-out hover:scale-150 hover:text-gray-700" size={32} />
-                <h3 className="text-lg md:text-xl font-bold text-black tracking-wide">ROI-Driven Integration</h3>
-                <p className="text-black text-lg">Our strategic approach to Play HQ integration focuses on delivering maximum return on investment. We ensure every aspect of the integration process contributes to your club's growth and success.</p>
+                <FaBasketballBall className="text-black transition-all duration-500 ease-in-out hover:scale-150 hover:text-gray-700" size={32} />
+                <h3 className="text-lg md:text-xl font-bold text-black tracking-wide">Seamless Integration</h3>
+                <p className="text-black text-lg">Our Play HQ integration allows you to seamlessly add fixtures and season stats to your website, providing your members with easy access to important information.</p>
             </div>
             <div className="integration-item text-left p-6 md:p-10 space-y-4 transition-all duration-500 ease-in-out transform hover:shadow-2xl hover:scale-105">
-                <FaCheck className="text-black transition-all duration-500 ease-in-out hover:scale-150 hover:text-gray-700" size={32} />
-                <h3 className="text-lg md:text-xl font-bold text-black tracking-wide">Performance Tracking</h3>
-                <p className="text-black text-lg">With our integration services, you gain access to real-time performance reports. These insights help you understand your digital growth and make informed decisions for your club's future.</p>
+                <FaCalendarAlt className="text-black transition-all duration-500 ease-in-out hover:scale-150 hover:text-gray-700" size={32} />
+                <h3 className="text-lg md:text-xl font-bold text-black tracking-wide">Real-Time Updates</h3>
+                <p className="text-black text-lg">With real-time updates, your members can stay up-to-date with the latest fixtures and stats, improving their experience and engagement.</p>
             </div>
             <div className="integration-item text-left p-6 md:p-10 space-y-4 transition-all duration-500 ease-in-out transform hover:shadow-2xl hover:scale-105">
-                <FaRocket className="text-black transition-all duration-500 ease-in-out hover:scale-150 hover:text-gray-700" size={32} />
-                <h3 className="text-lg md:text-xl font-bold text-black tracking-wide">Growth-Oriented Integration</h3>
-                <p className="text-black text-lg">At Ordinary Agency, we're committed to helping Perth sports clubs reach new heights. Our Play HQ integration services are designed to elevate your club's efficiency, improve member satisfaction, and drive growth.</p>
+                <FaWhistle className="text-black transition-all duration-500 ease-in-out hover:scale-150 hover:text-gray-700" size={32} />
+                <h3 className="text-lg md:text-xl font-bold text-black tracking-wide">Efficient Management</h3>
+                <p className="text-black text-lg">Play HQ integration simplifies the management of your sports club, allowing you to focus on what matters most - the game.</p>
             </div>
           </div>
         </section>
