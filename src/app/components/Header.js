72e8afa9be244554d6c296fb747943869379e4d9
Emmett Hoolahan <emmetthoolahan@gmail.com>
2024-01-23 17:20:01 +0800

    phone

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -125,7 +125,7 @@ const Header = () => {
            </a>
          </div>
          <div className="hidden md:flex mr-2 mb-2 ml-2 items-center justify-center">
-           <a href="tel:+610899308081" className="bg-white text-black text-sm font-bold py-2 px-4 rounded-full transition-colors duration-300 hover:bg-gray-200 text-lg">
+           <a href="tel:+610899308081" className={`text-sm font-bold py-2 px-4 rounded-full transition-colors duration-300 text-lg ${isScrolled ? 'bg-black text-white' : 'bg-white text-black'} hover:bg-gray-200`}>
              <FiPhone className="inline mr-2" />
              (08) 9930 8081
            </a>
